f3e4cbe0ed1c65bccbcbd9ca12f14186a894a57b
kev2108 <kevindanieldana@gmail.com>
2024-05-27 14:07:52 -0300

    CommitAccess

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class LoginManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public InputField Input_Password;
+    string contraseña = "kevin";
+    // Start is called before the first frame update
+    public void BotonPresionar()
     {
-        
+        if (Input_Password.text == contraseña)
+        {
+            Debug.Log("Access granted");
+        }
+        else
+        {
+            Debug.Log("Access denied");
+        }
     }
 }
